59eaf52169cb06feec95e85609662e5072759d78
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-27 22:48:48 +0400

    Create User.java

---
+++ src/main/java/com/rustam/Movie_Website/dao/entity/User.java
@@ -0,0 +1,47 @@
+package com.rustam.Movie_Website.dao.entity;
+
+import com.rustam.Movie_Website.dao.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+    private String name;
+    private String surname;
+    private String username;
+    private String password;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @JoinTable(name = "authorities", joinColumns = @JoinColumn(name = "base_user_id"))
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    Set<Role> authorities;
+
+
+    public String getId() {
+        if (id != null) {
+            return id.toString();
+        }
+        return null;
+    }
+}
