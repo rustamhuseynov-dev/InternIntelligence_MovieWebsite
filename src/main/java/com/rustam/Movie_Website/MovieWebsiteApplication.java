504699b344c2dcc571eb9bd848336e1ab33c4532
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-27 16:26:20 +0400

    Create MovieWebsiteApplication.java

---
+++ src/main/java/com/rustam/Movie_Website/MovieWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.rustam.Movie_Website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWebsiteApplication.class, args);
+	}
+
+}
