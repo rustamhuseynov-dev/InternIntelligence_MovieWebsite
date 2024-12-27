c2f36d816d7875ef2a6966e760af31cd2e4dccb8
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-27 22:49:16 +0400

    Create UserRegisterResponse.java

---
+++ src/main/java/com/rustam/Movie_Website/dto/response/UserRegisterResponse.java
@@ -0,0 +1,17 @@
+package com.rustam.Movie_Website.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRegisterResponse {
+
+    private String name;
+    private String surname;
+    private String username;
+}
