1874fe204f4069df215aa24d18e1bc255d84b87c
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-27 22:49:07 +0400

    Create AuthRequest.java

---
+++ src/main/java/com/rustam/Movie_Website/dto/request/AuthRequest.java
@@ -0,0 +1,17 @@
+package com.rustam.Movie_Website.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
