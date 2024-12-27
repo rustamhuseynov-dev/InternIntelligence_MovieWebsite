0ba3598ba1ebf91eeecc844afbceed694b3129d0
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-27 16:26:28 +0400

    Create MovieWebsiteApplicationTests.java

---
+++ src/test/java/com/rustam/Movie_Website/MovieWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rustam.Movie_Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
