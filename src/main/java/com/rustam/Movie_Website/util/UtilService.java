bed82c100a7f3de0a62c88dbf73e39edbd6ec451
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-28 11:16:49 +0400

    Update UtilService.java

--- src/main/java/com/rustam/Movie_Website/util/UtilService.java
+++ src/main/java/com/rustam/Movie_Website/util/UtilService.java
@@ -3,10 +3,14 @@ package com.rustam.Movie_Website.util;
 import com.rustam.Movie_Website.dao.entity.User;
 import com.rustam.Movie_Website.dao.repository.UserRepository;
 import com.rustam.Movie_Website.dto.TokenPair;
+import com.rustam.Movie_Website.dto.request.RefreshRequest;
 import com.rustam.Movie_Website.exception.custom.InvalidUUIDFormatException;
+import com.rustam.Movie_Website.exception.custom.UnauthorizedException;
 import com.rustam.Movie_Website.exception.custom.UserNotFoundException;
+import com.rustam.Movie_Website.util.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
@@ -21,6 +25,7 @@ public class UtilService {
 
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
+    private final RedisTemplate<String,String> redisTemplate;
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username)
@@ -52,5 +57,21 @@ public class UtilService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public String refreshToken(RefreshRequest request) {
+        System.out.println(request.getRefreshToken());
+        log.info("api-gateway to send refresh token {}",request.getRefreshToken());
+        String userId = jwtUtil.getUserIdAsUsernameFromTokenExpired(request.getRefreshToken()); // Token-dan user ID-ni çıxart
+        if (userId == null) {
+            throw new UnauthorizedException("Invalid refresh token");
+        }
+        String redisKey = "refresh_token:" + userId; // Redis açarını yaradın
+        String storedRefreshToken = redisTemplate.opsForValue().get(redisKey); // Redis-dən saxlanmış refresh token-i alın
+        if (storedRefreshToken != null) {
+            return jwtUtil.createToken(userId);
+        } else {
+            throw new UnauthorizedException("Invalid refresh token");
+        }
     }
 }
