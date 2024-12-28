b5a00a6ba1d743db6d7816335b95f78cce3c4b1c
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-28 11:48:06 +0400

    Create MovieDeletedResponse.java

---
+++ src/main/java/com/rustam/Movie_Website/dto/response/MovieDeletedResponse.java
@@ -0,0 +1,20 @@
+package com.rustam.Movie_Website.dto.response;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MovieDeletedResponse {
+    private Long id;
+    private String title;
+    private String director;
+    private String releaseYear;
+    private String genre;
+    private Double IMDb;
+}
