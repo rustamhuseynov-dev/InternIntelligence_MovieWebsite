14a164cdc5098551fa78e4cc4761fedcb52aac8a
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-28 11:48:08 +0400

    Create MovieReadResponse.java

---
+++ src/main/java/com/rustam/Movie_Website/dto/response/MovieReadResponse.java
@@ -0,0 +1,13 @@
+package com.rustam.Movie_Website.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MovieReadResponse {
+}
