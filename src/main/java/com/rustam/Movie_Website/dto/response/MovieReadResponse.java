6728a3b8abae5dff42778fb0634801fe7b1ef903
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-28 15:36:41 +0400

    Update MovieReadResponse.java

--- src/main/java/com/rustam/Movie_Website/dto/response/MovieReadResponse.java
+++ src/main/java/com/rustam/Movie_Website/dto/response/MovieReadResponse.java
@@ -1,5 +1,6 @@
 package com.rustam.Movie_Website.dto.response;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,4 +11,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class MovieReadResponse {
+    private String title;
+    private String director;
+    private String releaseYear;
+    private String genre;
+    private Double IMDb;
 }
