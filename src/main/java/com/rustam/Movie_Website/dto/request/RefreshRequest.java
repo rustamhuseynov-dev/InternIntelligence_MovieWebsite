39b83cf11a0c41d3e95608fd47ecc37a6dba2580
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-28 11:16:29 +0400

    Create RefreshRequest.java

---
+++ src/main/java/com/rustam/Movie_Website/dto/request/RefreshRequest.java
@@ -0,0 +1,15 @@
+package com.rustam.Movie_Website.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RefreshRequest {
+
+    private String refreshToken;
+}
