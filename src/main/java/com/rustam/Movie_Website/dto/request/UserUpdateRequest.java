53362b445bb44a5d586f7abe93224bc5b41bcd6b
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-28 10:13:53 +0400

    Create UserUpdateRequest.java

---
+++ src/main/java/com/rustam/Movie_Website/dto/request/UserUpdateRequest.java
@@ -0,0 +1,20 @@
+package com.rustam.Movie_Website.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserUpdateRequest {
+
+    private UUID id;
+    private String name;
+    private String surname;
+    private String username;
+}
