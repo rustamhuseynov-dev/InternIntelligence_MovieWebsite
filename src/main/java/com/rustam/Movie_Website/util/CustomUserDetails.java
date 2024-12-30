1823364ad8e372cc26c871858649aa5e5e6512f1
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-30 22:58:56 +0400

    Update CustomUserDetails.java

--- src/main/java/com/rustam/Movie_Website/util/CustomUserDetails.java
+++ src/main/java/com/rustam/Movie_Website/util/CustomUserDetails.java
@@ -1,26 +1,26 @@
 package com.rustam.Movie_Website.util;
 
-import com.rustam.Movie_Website.dao.entity.User;
+import com.rustam.Movie_Website.dao.entity.BaseUser;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
-public record CustomUserDetails(User user) implements UserDetails {
+public record CustomUserDetails(BaseUser baseUser) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getAuthorities();
+        return baseUser.getAuthorities();
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return baseUser.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return user.getId();
+        return baseUser.getId();
     }
 
     @Override
