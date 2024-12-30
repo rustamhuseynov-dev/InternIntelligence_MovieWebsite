47e57366415a77e8040d409caed9eaa3e0a87e49
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-30 22:58:50 +0400

    Update UserDetailsServiceImpl.java

--- src/main/java/com/rustam/Movie_Website/service/UserDetailsServiceImpl.java
+++ src/main/java/com/rustam/Movie_Website/service/UserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.rustam.Movie_Website.service;
 
+import com.rustam.Movie_Website.dao.entity.BaseUser;
 import com.rustam.Movie_Website.dao.entity.User;
+import com.rustam.Movie_Website.dao.repository.BaseUserRepository;
 import com.rustam.Movie_Website.dao.repository.UserRepository;
 import com.rustam.Movie_Website.util.CustomUserDetails;
 import com.rustam.Movie_Website.util.UtilService;
@@ -14,14 +16,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final BaseUserRepository baseUserRepository;
     private final UtilService utilService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findById(utilService.convertToUUID(username))
+        BaseUser baseUser = baseUserRepository.findById(utilService.convertToUUID(username))
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
 
-        return new CustomUserDetails(user);
+        return new CustomUserDetails(baseUser);
     }
 }
