5ffdea799aa8b82db904a2d8a666ada1df649807
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-30 23:29:11 +0400

    Update User.java

--- src/main/java/com/rustam/Movie_Website/dao/entity/User.java
+++ src/main/java/com/rustam/Movie_Website/dao/entity/User.java
@@ -1,13 +1,9 @@
 package com.rustam.Movie_Website.dao.entity;
 
-import com.rustam.Movie_Website.dao.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.GenericGenerator;
 
-import java.util.Set;
-import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
