0ac9e257d075af080fb14be1207be867da12672f
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-12-30 22:58:36 +0400

    Update User.java

--- src/main/java/com/rustam/Movie_Website/dao/entity/User.java
+++ src/main/java/com/rustam/Movie_Website/dao/entity/User.java
@@ -2,47 +2,20 @@ package com.rustam.Movie_Website.dao.entity;
 
 import com.rustam.Movie_Website.dao.enums.Role;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.util.Set;
 import java.util.UUID;
 
-@Entity
-@Table(name = "users")
+@EqualsAndHashCode(callSuper = true)
 @Data
-@AllArgsConstructor
+@Entity
+@DiscriminatorValue("USER")
+@SuperBuilder
 @NoArgsConstructor
-@Builder
-public class User {
-
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    private UUID id;
-    private String name;
-    private String surname;
-    @Column(unique = true)
+@AllArgsConstructor
+public class User extends BaseUser{
     private String username;
-    private String password;
-
-    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
-    @JoinTable(name = "authorities", joinColumns = @JoinColumn(name = "base_user_id"))
-    @Column(name = "role", nullable = false)
-    @Enumerated(EnumType.STRING)
-    Set<Role> authorities;
-
-
-    public String getId() {
-        if (id != null) {
-            return id.toString();
-        }
-        return null;
-    }
 }
