0f174c1353e540391fea8ae98198a0bfe830531f
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-01 14:23:03 +0400

    Update UserMapper.java

--- src/main/java/com/rustam/Movie_Website/mapper/UserMapper.java
+++ src/main/java/com/rustam/Movie_Website/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.rustam.Movie_Website.mapper;
 
 import com.rustam.Movie_Website.dao.entity.BaseUser;
 import com.rustam.Movie_Website.dao.entity.User;
+import com.rustam.Movie_Website.dto.response.AdminRegisterResponse;
 import com.rustam.Movie_Website.dto.response.UserRegisterResponse;
 import com.rustam.Movie_Website.dto.response.UserUpdateResponse;
 import org.mapstruct.InjectionStrategy;
@@ -24,4 +25,6 @@ public interface UserMapper {
     List<UserRegisterResponse> toResponses(List<BaseUser> baseUsers);
 
     UserUpdateResponse toUpdated(BaseUser baseUser);
+
+    AdminRegisterResponse toAdminResponse(BaseUser user);
 }
