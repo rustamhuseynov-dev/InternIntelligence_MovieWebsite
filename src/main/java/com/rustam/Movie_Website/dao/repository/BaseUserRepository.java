4dce505581f8430241d9fa8f19048443ee89289f
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-01 14:22:45 +0400

    Update BaseUserRepository.java

--- src/main/java/com/rustam/Movie_Website/dao/repository/BaseUserRepository.java
+++ src/main/java/com/rustam/Movie_Website/dao/repository/BaseUserRepository.java
@@ -2,15 +2,19 @@ package com.rustam.Movie_Website.dao.repository;
 
 import com.rustam.Movie_Website.dao.entity.BaseUser;
 
+import com.rustam.Movie_Website.dao.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BaseUserRepository extends JpaRepository<BaseUser, UUID> {
-    @Query(value = "SELECT * FROM base_users WHERE (user_type = 'USER' OR user_type = 'ADMIN') AND username = :username", nativeQuery = true)
-    Optional<BaseUser> findByUsername(String username);
+    @Query(value = "SELECT * FROM base_users WHERE user_type = 'USER' AND username = :username", nativeQuery = true)
+    Optional<User> findByUsername(String username);
+
+    List<User> findAllBy();
 }
